722fb2cac276751a6e8b5aa31e4aad5dfb370fdd
Juha Tauriainen <juha@sc5.io>
2013-05-28 17:59:15 +0300

    First round of solutions from hackathon session at Kamppi office

--- test/test.js
+++ test/test.js
@@ -10,6 +10,34 @@ describe("URL parser", function() {
     });
 
     it("picks up host part", function() {
-        // ...
-    })
+        [
+          'www.google.fi',
+          'www.sc5.fi'
+        ].
+        forEach(function (hostName) {
+          assert.equal(hostName, URLParser.parse("http://" + hostName).hostName)
+        });
+    });
+
+    it("doesn't break on invalid url", function() {
+      assert.throws(URLParser.parse, Error);
+      assert.throws(function() {URLParser.parse({})}, Error);
+      assert.throws(function() {URLParser.parse(666)}, Error);
+      assert.throws(function() {URLParser.parse("")}, Error);
+    });
+
+    it("picks up protocol", function() {
+      [
+        'http', 'https', 'ftp', 'gopher'
+      ].
+      forEach(function (protocol) {
+        assert.equal(protocol, URLParser.parse(protocol + "://www.google.fi/").protocol)
+      });
+    });
+
+    it("finds tld", function() {
+      assert.equal('fi', URLParser.parse('http://www.google.fi').tld);
+    });
+
+
 });
